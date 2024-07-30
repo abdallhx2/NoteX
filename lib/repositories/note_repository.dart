4ee46c1d724f85de7832ae1b0987ca2505f9e7dc
abdallhx2 <3bdallhx2@gmail.com>
2024-07-30 18:01:46 +0300

    k

--- lib/repositories/note_repository.dart
+++ lib/repositories/note_repository.dart
@@ -1,6 +1,5 @@
-
-
 import 'package:notex/models/note.dart';
+
 abstract class NoteRepository {
   Future<List<Note>> getNotes();
   Future<void> addNote(Note note);
@@ -8,8 +7,5 @@ abstract class NoteRepository {
   Future<void> deleteNote(String id);
   Future<Note?> getNoteById(String id);
   Future<List<Note>> searchNotes(String query);
-  Future<void> deleteNote(String id) async {
-    await _syncService.deleteNoteFromCloud(id); // تأكد من تنفيذ الحذف في Firestore
-    // يمكن إضافة كود لحذف الملاحظة محلياً إذا لزم الأمر
-  }
+  // يمكن إضافة كود لحذف الملاحظة محلياً إذا لزم الأمر
 }
